c83e70ca0a0e80f41c5272f45c3a29c160f10659
choosak kwanraksri <btk1392545@gmail.com>
2024-06-30 16:03:26 +0700

    add cors in backend

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,21 +1,29 @@
-import { Rooms, Users } from "../Mockup/database";
+import { useEffect, useState } from "react";
+import { getAllRooms } from "../api/apiFunctions";
 
 function Home() {
-  const getUser = (id) => {
-    let res = Users.find((e) => e.id === id);
-    return res;
-  };
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    getAllRooms().then(res => setData(res))
+    
+  }, []);
 
   return (
     <div className="container mx-auto">
       <div className="max-w-2xl mx-auto space-y-2 gap-2 grid grid-cols-3 p-2">
-        {Rooms.map((data, i) => (
-          <div className="p-2 shadow-md rounded-md space-y-4" key={i}>
+        {data.map((data) => (
+          <div className="p-2 shadow-md rounded-md space-y-4" key={data._id}>
             <p>{data.room_name}</p>
             {data.users.map((user) => (
-              <div className="flex items-center space-x-2" key={user}>
-                <img className="w-10 rounded-full h-10 object-cover" src={getUser(user).image} alt="profile" />
-                <p className="text-xs">{getUser(user).name}</p>
+              <div className="flex items-center space-x-2" key={user._id}>
+                <img
+                  className="w-10 rounded-full h-10 object-cover"
+                  src={user.image}
+                  alt="profile"
+                />
+                <p className="text-xs">{user.name}</p>
               </div>
             ))}
           </div>
---
+++ frontend/src/config.js
@@ -0,0 +1,6 @@
+const config = {
+    apiPrefix: 'https://virtual-campus-coe-41qi.vercel.app/api',
+}
+
+
+export default config
