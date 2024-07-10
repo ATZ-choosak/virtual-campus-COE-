b9fa16201eceacd6be210438cd1f937e28a463f8
choosak kwanraksri <btk1392545@gmail.com>
2024-07-11 00:57:18 +0700

    update api

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
+const isProd = process.env.NODE_ENV === 'production'
 
 const config = {
-    apiPrefix: '/api',
-    authPrefix: '/auth',
+    apiPrefix: isProd ? '/api' : "http://localhost:6455/api",
+    authPrefix: isProd ? '/auth' : "http://localhost:6455/auth",
 }
 
 export default config
