5859539f474e54597afa0462667fc44759f20c5f
choosak kwanraksri <btk1392545@gmail.com>
2024-07-11 01:26:59 +0700

    update api

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,12 @@ app.use(
     })
 );
 app.use(express.json({ limit: '5mb' }));
-app.use(express.static('/home/atozer/Desktop/uploads'));
+
+// Define the path to your uploads directory
+const uploadsPath = path.join('/home', 'atozer', 'Desktop', 'uploads');
+
+// Serve static files from the uploads directory
+app.use(express.static(uploadsPath));
 
 //Use Router
 app.use("/api", roomRouter)
