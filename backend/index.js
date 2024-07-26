9b081309dd8460e545aa8e91850496c132606bd1
choosak kwanraksri <btk1392545@gmail.com>
2024-07-26 22:39:03 +0700

    change cors

--- backend/index.js
+++ backend/index.js
@@ -15,13 +15,14 @@ const authRouter = require("./router/authRouter")
 const imageRouter = require("./router/imageRouter")
 
 // Middleware to parse JSON bodies
-app.use(
-    cors({
-        origin: process.env.PRODUCTION ? "*" : "http://localhost:3000",
-        methods: "GET,POST,PUT,DELETE",
-        credentials: true,
-    })
-);
+// app.use(
+//     cors({
+//         origin: process.env.PRODUCTION ? "*" : "http://localhost:3000",
+//         methods: "GET,POST,PUT,DELETE",
+//         credentials: true,
+//     })
+// );
+app.use(cors())
 app.use(express.json({ limit: '5mb' }));
 app.use(express.static("/home/atozer/Desktop/uploads"));
 
