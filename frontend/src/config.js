301182433343e7c7a2157244206f99f4149939d1
choosak kwanraksri <btk1392545@gmail.com>
2024-07-31 21:56:50 +0700

    change image path

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const config = {
     apiPrefix: isProd ? '/api' : "http://localhost:6455/api",
     authPrefix: isProd ? '/auth' : "http://localhost:6455/auth",
-    fileApiPrefix: isProd ? '/' : "http://localhost:6455",
+    fileApiPrefix: isProd ? '' : "http://localhost:6455",
 }
 
 export default config
