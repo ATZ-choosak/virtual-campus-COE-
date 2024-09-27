f99abdf1ee0431ce6c15054d63aa76b4c2aae773
choosak kwanraksri <btk1392545@gmail.com>
2024-09-27 18:04:47 +0700

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const imageRouter = require("./router/imageRouter")
 // );
 app.use(cors())
 app.use(express.json({ limit: '5mb' }));
-app.use("/uploads", express.static(process.env.NODE_ENV === "development" ? path.join(__dirname, "/uploads") : "/home/atozer/Desktop/uploads"));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 //Use Router
 app.use("/api", roomRouter)
